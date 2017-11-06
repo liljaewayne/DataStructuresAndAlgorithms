2b287eb6a8b50b54380567b03ecacc0f5f4af474
HTYUNLU\liujianwei <liujianwei@htyunwang.com>
2017-11-06 14:37:21 +0800

    增加JFrame HelloWorld

---
+++ visual-algorithms/src/main/java/com/visual/jframe/Hello.java
@@ -0,0 +1,10 @@
+package com.visual.jframe;
+
+import javax.swing.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+    }
+}
