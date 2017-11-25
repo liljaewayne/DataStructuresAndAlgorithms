81305ee014cb94194dbde659b100231fc5430533
liljaewayne <792068007@qq.com>
2017-11-25 15:46:21 +0800

    将Main函数去掉

--- visual-algorithms/src/main/java/com/visual/AlgoVisualizer.java
+++ visual-algorithms/src/main/java/com/visual/AlgoVisualizer.java
@@ -47,4 +47,14 @@ public class AlgoVisualizer {
         }
     }
 
+
+    public static void main(String[] args) {
+
+        int sceneWidth = 800;
+        int sceneHeight = 800;
+        int N = 10;
+
+        AlgoVisualizer visualizer = new AlgoVisualizer(sceneWidth, sceneHeight, N);
+
+    }
 }
