dc8642250c4cedcf75afc5a6705df4e8c9d192e1
liljaewayne <792068007@qq.com>
2017-11-30 16:19:33 +0800

    深度优先遍历使用栈辅助迭代实现生成迷宫

---
+++ visualizationAlgorithm/src/main/java/com/_6/dfs/maze/generation/adt/Position.java
@@ -0,0 +1,19 @@
+package com._6.dfs.maze.generation.adt;
+
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
