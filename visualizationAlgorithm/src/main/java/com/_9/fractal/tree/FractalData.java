444bb769c879de6ca959de914518adb0168a081c
liljaewayne <792068007@qq.com>
2017-12-04 20:50:54 +0800

    绘制树形分形图, 是通过线的分形完成树状的形态

---
+++ visualizationAlgorithm/src/main/java/com/_9/fractal/tree/FractalData.java
@@ -0,0 +1,12 @@
+package com._9.fractal.tree;
+
+public class FractalData {
+
+    public int depth;
+    public double splitAngle;
+
+    public FractalData(int depth, double splitAngle) {
+        this.depth = depth;
+        this.splitAngle = splitAngle;
+    }
+}
